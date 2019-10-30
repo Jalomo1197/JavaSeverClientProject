c59f041ba008e8dd1898d2f3bbf9d3679707a60b
Jalomo1197 <alexisjalomo1197@gmail.com>
2019-10-29 20:23:04 -0500

    server files updates

--- P3Server/src/main/java/RPLS.java
+++ P3Server/src/main/java/RPLS.java
@@ -1,9 +1,21 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 public class RPLS extends Application {
+	Server serverConnection;
+	Scene startUp_scene, info_scene;
+	VBox LayoutOne;
+	HBox subLayoutOne;
+	TextField portNumber;
+	Button enterPortNumber_btn, turnOnSever_btn;
+	Text gameServer_text, portNumPrompt_text, connectedClients_text, serverInfo_text, winner_text;
+	ListView information_listView;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -14,11 +26,36 @@ public class RPLS extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
+		initTextVars();
+		initButtonVars();
+		initOtherVars();
+
+
 		primaryStage.setTitle("RPLS!!!");
-		
+
+
+
 		Scene scene = new Scene(new HBox(),600,600);
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+
+	void initTextVars(){
+		gameServer_text = new Text("Game Server");
+		portNumPrompt_text = new Text("Enter port number to listen to: ");
+		connectedClients_text = new Text("Number of clients connected: " + 0);
+		serverInfo_text = new Text("Server Information: ");
+		winner_text = new Text("Winner: ");
+	}
+
+	void initButtonVars(){
+		enterPortNumber_btn = new Button("Enter");
+		turnOnSever_btn = new Button("Turn on server");
+	}
+
+	void initOtherVars(){
+		portNumber = new TextField("e.g. 5555");
+		information_listView = new ListView();
 	}
 
 }
